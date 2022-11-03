91cb54c153f2a39ff928d60ce052c8599a14bf12
RichardChileya <richardchileyawk@gmail.com>
2022-11-03 12:11:42 +0200

    Update App script

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Quote from './components/Quote';
+import Home from './components/Home';
+import Header from './components/Header';
 import Calc from './components/Calculator';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Calc />
-    );
-  }
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calc />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
