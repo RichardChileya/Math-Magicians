f586ecf0cada46a99554d576c88ac9a83f4294c8
RichardChileya <richardchileyawk@gmail.com>
2022-11-03 12:15:02 +0200

    Add Rows Updates

--- src/components/Rows.js
+++ src/components/Rows.js
@@ -1,28 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-class Rows extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.buttonClicks = this.buttonClicks.bind(this);
+const Rows = ({ row, buttonClicks }) => {
+  // const handleClicks = (e) => {
+  //   buttonClicks(e.target.innerText);
+  // }
+  const buttons = [];
+  for (let i = 0; i < row.length; i += 1) {
+    buttons.push(<button type="button" key={row[i]} value={row[i]} onClick={buttonClicks}>{row[i]}</button>);
   }
-
-  buttonClicks(e) {
-    const { buttonClicks } = this.props;
-    buttonClicks(e.target.innerText);
-  }
-
-  render() {
-    const { row } = this.props;
-    const buttons = [];
-    for (let i = 0; i < row.length; i += 1) {
-      buttons.push(<button type="button" key={i} onClick={this.buttonClicks}>{ row[i] }</button>);
-    }
-
-    return buttons;
-  }
-}
+  return buttons;
+};
 
 Rows.propTypes = {
   row: PropTypes.arrayOf(PropTypes.string).isRequired,
